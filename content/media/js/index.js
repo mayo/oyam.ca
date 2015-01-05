150b81f552f9b52d0f5f2e86c0b0a7417e021ac9
Mayo Jordanov <mayo@oyam.ca>
2015-01-04 21:32:37 -0800

    Add touch and key events. Add comments.
    Code jibbed from baguetteBox.js: https://github.com/feimosi/baguetteBox.js/blob/master/src/baguetteBox.js

--- content/media/js/index.js
+++ content/media/js/index.js
@@ -1,4 +1,6 @@
 
+/* simple slideshow logic */
+
 function Slideshow(selector) {
   //fire up some events
 
@@ -49,12 +51,14 @@ function Slideshow(selector) {
 
 MicroEvent.mixin(Slideshow);
 
+/* website specific */
 var s;
 
 document.addEventListener('DOMContentLoaded', function() {
 
   s = new Slideshow("#frontpage-slideshow .slide");
 
+  /* change up some website styles depending on which slide is being shown */
   var callback = function(slide) {
     var darkbg = slide.hasAttribute('data-darkbg');
     var bodyTag = document.querySelector('html > body');
@@ -94,6 +98,7 @@ s
 
   s.showSlide(getSelectedSlide());
 
+  // Bind UI left/right arrows
   document.querySelector('#frontpage-slideshow .controls .previous')
     .addEventListener('click', function(e) {
       s.previous();
@@ -106,8 +111,63 @@ s
       e.preventDefault();
     }, false);
 
+  // React to changing anchor/hash in URL
   window.addEventListener("hashchange", function(e) {
     s.showSlide(getSelectedSlide());
   }, false);
+
+  document.addEventListener("keydown", function(e) {
+    // Do nothing when modifiers active. This prevents capturing keys when
+    // switching tabs, which can be very annoying.
+    if (e.altKey || e.altGraphKey || e.metaKey || e.shiftKey) return;
+
+    switch(e.keyCode) {
+      // Left arrow
+      case 37:
+        s.previous();
+        e.preventDefault();
+        break;
+      // Right arrow
+      case 39:
+        s.next();
+        e.preventDefault();
+        break;
+      // Esc
+      case 27:
+        hideOverlay();
+        break;
+    }
+  }, false);
+
+  var se = document.querySelectorAll("#frontpage-slideshow")[0];
+  var touchStartX;
+  var touchFlag;
+  var touchDisplacement = 60;
+
+  se.addEventListener('touchstart', function(e) {
+    touchStartX = e.changedTouches[0].pageX;
+  }, false);
+
+  se.addEventListener('touchmove', function(e) {
+    // If action was already triggered return
+    if (touchFlag) return;
+
+    e.preventDefault();
+
+    touch = e.touches[0] || e.changedTouches[0];
+    // Move at least 40 pixels to trigger the action
+    if(touch.pageX - touchStartX > touchDisplacement) {
+      touchFlag = true;
+      s.previous();
+
+    } else if (touch.pageX - touchStartX < -1 * touchDisplacement) {
+      touchFlag = true;
+      s.next();
+    }
+  }, false);
+
+  se.addEventListener('touchend', function(e) {
+    touchFlag = false;
+  });
 
 }, false);
