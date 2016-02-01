49c26b1ce4599e9822941f973d72af270a793efa
Mayo Jordanov <mayo@oyam.ca>
2016-01-31 19:18:42 -0800

    Add smooth scrolling function

--- content/media/js/index.js
+++ content/media/js/index.js
@@ -48,4 +48,67 @@ s
   s.bind('advance', callback);
   s.showSlide(se.selectedSlide());
 
+  //smooth scroll to element
+  function smoothScroll(element) {
+    var currentYPosition = function() {
+      // Firefox, Chrome, Opera, Safari
+      if (self.pageYOffset) return self.pageYOffset;
+      // Internet Explorer 6 - standards mode
+      if (document.documentElement && document.documentElement.scrollTop)
+          return document.documentElement.scrollTop;
+      // Internet Explorer 6, 7 and 8
+      if (document.body.scrollTop) return document.body.scrollTop;
+      return 0;
+    }
+
+    var elmYPosition = function(eID) {
+      var elm = document.getElementById(eID);
+      var y = elm.offsetTop;
+      var node = elm;
+      while (node.offsetParent && node.offsetParent != document.body) {
+          node = node.offsetParent;
+          y += node.offsetTop;
+      } return y;
+    }
+
+    var scroll = function(eID) {
+      var startY = currentYPosition();
+      var stopY = elmYPosition(eID);
+      var distance = stopY > startY ? stopY - startY : startY - stopY;
+      if (distance < 100) {
+          scrollTo(0, stopY); return;
+      }
+      var speed = Math.round(distance / 50);
+      if (speed >= 20) speed = 20;
+      var step = Math.round(distance / 25);
+      var leapY = stopY > startY ? startY + step : startY - step;
+      var timer = 0;
+      if (stopY > startY) {
+          for ( var i=startY; i<stopY; i+=step ) {
+              setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+              leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+          } return;
+      }
+      for ( var i=startY; i>stopY; i-=step ) {
+          setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+          leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+      }
+    }
+
+    return scroll(element);
+  }
+
+  document.querySelector('.scrolldown a[href*="#"]:not([href="#"])').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = null;
+
+      target = e.target && e.target.parentElement ? e.target.parentElement : null;
+      target = target.hash.length ? target.hash.slice(1) : null;
+
+      if (target) smoothScroll(target);
+    }
+  });
+
 }, false);
