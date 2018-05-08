cb8be017e4e27d4f0a55b9e31da40df49aff524d
Mayo Jordanov <mayo@oyam.ca>
2018-05-07 18:11:41 -0700

    Build updates
    - Switch to using encrypted environment file
    - Enable webmention detection

---
+++ tools/encrypt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+[ -z "${ENCRYPTION_KEY}" ] && echo "Missing encryption key" && exit 1
+[ -z "$1" ] && echo "Please provide a file to encrypt" && exit 1
+
+openssl aes-256-cbc -k "${ENCRYPTION_KEY}" -in "$1" -out "$1".enc
