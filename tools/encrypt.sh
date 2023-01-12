a6fcb9d85259aa4345621c91cfff41dc25e7755e
Mayo Jordanov <mayo@oyam.ca>
2023-01-11 18:25:19 -0800

    Use pbkdf2 key derivation

--- tools/encrypt.sh
+++ tools/encrypt.sh
@@ -3,4 +3,4 @@
 [ -z "${ENCRYPTION_KEY}" ] && echo "Missing encryption key" && exit 1
 [ -z "$1" ] && echo "Please provide a file to encrypt" && exit 1
 
-openssl aes-256-cbc -k "${ENCRYPTION_KEY}" -md sha256 -in "$1" -out "$1".enc
+openssl aes-256-cbc -k "${ENCRYPTION_KEY}" -md sha256 -pbkdf2 -in "$1" -out "$1".enc
